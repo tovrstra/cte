cdd6ea331ee443dbbfa0403b3cb43fde1dce60e7
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2023-11-11 17:11:37 +0100

    Update AY 23-24

--- README.md
+++ README.md
@@ -5,10 +5,7 @@ All files in this repository are licensed under a [Creative Commons Attribution-
 
 Download everything [as a ZIP file](https://github.com/tovrstra/cte/archive/refs/heads/main.zip)
 
-To run this notebook in your browser, use one of the two following links:
-
-1. [Open the notebook in Google Colab ![Google Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/tovrstra/cte/blob/main/demo.ipynb) (Code execution requires a Google account. Easy and high performance. Somewhat dated Python packages.)
-2. [Open the notebook in Binder ![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/tovrstra/cte/main?filepath=demo.ipynb) (No account required. Bleeding edge Python packages. Starts slowly and times out after 10 minutes of inactivity.)
+To run this notebook in your browser, [Open the notebook in Google Colab ![Google Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/tovrstra/cte/blob/main/demo.ipynb) (Code execution requires a Google account.)
 
 Also a Matlab version of the same code is provided in [demo.m](demo.m).
 It does not contain the presentation, just the Matlab translation of the code in section 4 of the notebook.
--- demo.m
+++ demo.m
@@ -1,49 +1,43 @@
 % All values are in SI base units.
-global STD_GRAVITY
-global COULOMB_CONSTANT
 global MASS
-global LENGTH
-global MOMENT_OF_INERTIA
+global FORCE_CONST
 global TIMESTEP
 global NSTEP
 
-STD_GRAVITY = 9.80665;
-COULOMB_CONSTANT = 8.9875517923e9;
 MASS = 0.062;
-LENGTH = 0.80;
-MOMENT_OF_INERTIA = MASS * LENGTH^2;
-TIMESTEP = 1.79 / 50;
+FORCE_CONST = 2.5;
+TIMESTEP = 0.01;
 NSTEP = 75;
 
 % Initialize some variables
-theta = pi/3;
-omega = 0.0;
+x = 0.3;
+v = 0.0;
 
 % Initialize zero arrays in which the trajectory is stored.
-thetas = 1: NSTEP;
-omegas = 1: NSTEP;
+xs = 1: NSTEP;
+vs = 1: NSTEP;
 
 % Store the initial state
-thetas(1) = theta;
-omegas(1) = omega;
+xs(1) = x;
+vs(1) = v;
 
 % Leapfrog integration
-ang_accel = torque_theta(theta) / MOMENT_OF_INERTIA;
+a = force(x) / MASS;
 for istep = 1: NSTEP
     % Leapfrog algorithm for a single step.
-    omega_half = omega + TIMESTEP * ang_accel / 2;
-    theta_next = theta + TIMESTEP * omega_half;
-    ang_accel_next = torque_theta(theta_next) / MOMENT_OF_INERTIA;
-    omega_next = omega_half + TIMESTEP * ang_accel_next / 2;
+    v_half = v + TIMESTEP * a / 2;
+    x_next = x + TIMESTEP * v_half;
+    a_next = force(x_next) / MASS;
+    v_next = v_half + TIMESTEP * a_next / 2;
 
     % Store results.
-    thetas(istep + 1) = theta_next;
-    omegas(istep + 1) = omega_next;
+    xs(istep + 1) = x_next;
+    vs(istep + 1) = v_next;
 
     % Assign "next" results to current.
-    theta = theta_next;
-    omega = omega_next;
-    ang_accel = ang_accel_next;
+    x = x_next;
+    v = v_next;
+    a = a_next;
 end
 
 % Plot of position, velocity and energies as a function of time.
@@ -52,24 +46,22 @@ ts = (1 : NSTEP + 1) * TIMESTEP;
 
 hold on;
 subplot(1, 3, 1)
-plot(ts, thetas)
-xlabel("time t [s]")
-ylabel("angle theta [rad]")
+plot(ts, xs)
+xlabel("t [s]")
+ylabel("x [m]")
 
 subplot(1, 3, 2)
-plot(ts, omegas)
-xlabel("time t [s]")
-ylabel("angular velocity omega [rad/s]")
+plot(ts, vs)
+xlabel("t [s]")
+ylabel("v [m/s]")
 
 subplot(1, 3, 3)
-eks = kinetic_energy(omegas);
-eps = potential_energy(thetas);
+eks = kinetic_energy(vs);
+eps = potential_energy(xs);
 plot(ts, eks, "b", ts, eps, "r", ts, eks + eps, "k")
-% plot(ts, eps)
-% plot(ts, eks + eps, 'color', "k")
 legend("kin. E", "pot. E", "total E")
-xlabel("time t [s]")
-ylabel("energy [J]")
+xlabel("t [s]")
+ylabel("Energy [J]")
 saveas(gca(), "matlab_trajectory.png", "png")
 clf()
 hold off;
@@ -78,32 +70,32 @@ hold off;
 % Plot velocity versus position
 
 hold on;
-plot(thetas, omegas)
-xlabel("angle theta [rad]")
-ylabel("angular velocity omega [rad/s]")
-saveas(gca(), "matlab_omega_theta.png", "png")
+plot(xs, vs)
+xlabel("x [m]")
+ylabel("v [m/s]")
+saveas(gca(), "matlab_v_x.png", "png")
 clf()
 hold off;
 
 
 % Plot contour lines for the total energy
 
-theta_low = min(thetas);
-theta_high = max(thetas);
-theta_margin = (theta_high - theta_low) * 0.15;
-theta_low = theta_low - theta_margin;
-theta_high = theta_high + theta_margin;
-omega_low = min(omegas);
-omega_high = max(omegas);
-omega_margin = (omega_high - omega_low) * 0.15;
-omega_low = omega_low - omega_margin;
-omega_high = omega_high + omega_margin;
+x_low = min(thetas);
+x_high = max(thetas);
+x_margin = (x_high - x_low) * 0.15;
+x_low = x_low - x_margin;
+x_high = x_high + x_margin;
+v_low = min(omegas);
+v_high = max(omegas);
+v_margin = (v_high - v_low) * 0.15;
+v_low = v_low - v_margin;
+v_high = v_high + v_margin;
 hold on;
-[theta_grid, omega_grid] = meshgrid(linspace(theta_low, theta_high, 51), linspace(omega_low, omega_high, 51));
-energy_grid = kinetic_energy(omega_grid) + potential_energy(theta_grid);
-contour(theta_grid, omega_grid, energy_grid, 40)
-xlabel("angle theta [rad]")
-ylabel("angular velocity omega [rad/s]")
+[x_grid, v_grid] = meshgrid(linspace(x_low, x_high, 51), linspace(v_low, v_high, 51));
+energy_grid = kinetic_energy(v_grid) + potential_energy(x_grid);
+contour(x_grid, v_grid, energy_grid, 40)
+xlabel("x [m]")
+ylabel("v [m/s]")
 saveas(gca(), "matlab_ener_contour.png", "png")
 clf()
 hold off;
@@ -111,21 +103,17 @@ hold off;
 % Functions must be defined in the end, in Matlab,
 % unlike in any other programming language...
 
-function [out] = torque_theta(theta)
-    global MASS
-    global STD_GRAVITY
-    global LENGTH
-    out = -MASS * STD_GRAVITY * LENGTH * sin(theta);
+function [out] = force(x)
+    global FORCE_CONST
+    out = -FORCE_CONST * x;
 end
 
-function [out] = potential_energy(theta)
-    global MASS
-    global STD_GRAVITY
-    global LENGTH
-    out = MASS * STD_GRAVITY * LENGTH * (1 - cos(theta));
+function [out] = potential_energy(x)
+    global FORCE_CONST
+    out = (0.5 * FORCE_CONST) * x.^2;
 end
 
-function [out] = kinetic_energy(omega)
-    global MOMENT_OF_INERTIA
-    out = (0.5 * MOMENT_OF_INERTIA) * omega.^2;
+function [out] = kinetic_energy(v)
+    global MASS
+    out = (0.5 * MASS) * v.^2;
 end
